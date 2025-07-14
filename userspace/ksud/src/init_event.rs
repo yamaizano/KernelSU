a84d5b045070b398c35adb04a0f44280be8971a5
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-07-14 18:13:29 +0800

    ksud: remove tmpfs mounting
    if a user wants it, they should just mount it themselves

--- userspace/ksud/src/init_event.rs
+++ userspace/ksud/src/init_event.rs
@@ -1,4 +1,4 @@
-use crate::defs::{KSU_MOUNT_SOURCE, NO_MOUNT_PATH, NO_TMPFS_PATH};
+use crate::defs::{KSU_MOUNT_SOURCE, NO_MOUNT_PATH, TEMP_DIR};
 use crate::module::{handle_updated_modules, prune_modules};
 use crate::{assets, defs, ksucalls, restorecon, utils};
 use anyhow::{Context, Result};
@@ -75,21 +75,6 @@ pub fn on_post_data_fs() -> Result<()> {
 
     if let Err(e) = crate::profile::apply_sepolies() {
         warn!("apply root profile sepolicy failed: {e}");
-    }
-
-    // mount temp dir
-    if !Path::new(NO_TMPFS_PATH).exists() {
-        if let Err(e) = mount(
-            KSU_MOUNT_SOURCE,
-            utils::get_tmp_path(),
-            "tmpfs",
-            MountFlags::empty(),
-            "",
-        ) {
-            warn!("do temp dir mount failed: {}", e);
-        }
-    } else {
-        info!("no tmpfs requested");
     }
 
     // exec modules post-fs-data scripts
